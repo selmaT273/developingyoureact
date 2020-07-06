bd887f3375d42653bc45413e775feea946d118d8
francescodt <francescodtroiano@gmail.com>
2020-07-06 13:32:15 -0500

    adding footer structure

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,2 +1,13 @@
 import React from 'react';
 
+import './footer.css';
+
+export default function Footer() {
+    
+    return(
+      <footer>
+        <h1>This is the footer</h1>
+        
+      </footer>
+    );
+  }
