7d8c5d2f7b09477dc6742ce681b1dd3e609ad4c1
Stacey Teltser <stacey.teltser@gmail.com>
2020-07-06 14:40:13 -0500

    renamed files to scss and started building nav bar

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import './footer.css';
+// import './footer.sass';
 
 export default function Footer() {
     
     return(
       <footer>
-        <h1>This is the footer</h1>
+        <h1>ATTN Productions</h1>
         
       </footer>
     );
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
+// import link from 'react-router-dom';
 
-//import './header.css';
+import './header.scss';
 
 export default function Header() {
     
     return(
       <header>
-        <h1>This is the header.</h1>
-        
+        <h1>Developing You</h1>
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/Goals">Goals</a></li>
+            <li><a href="/About">About</a></li>
+          </ul>
+        </nav>
       </header>
     );
   }
