556c63848376c9d9622bfb1e887aac5bc643200f
cnickels21 <cnickels_21@yahoo.com>
2020-07-07 14:33:40 -0500

    input fields on form

--- src/creategoal/index.js
+++ src/creategoal/index.js
@@ -7,7 +7,21 @@ export default function CreateGoal(props){
         <>
             <h3>Create Your Goal!</h3>
             <form onSubmit={""}>
-                
+                <label>Title: </label>
+                <input 
+                    name="title" 
+                    placeholder="Goal title" 
+                    onChange={""} />
+                <label>Start Value: </label>
+                <input 
+                    name="startValue" 
+                    placeholder="What's your current rate?" 
+                    onChange={""} />
+                <label>Goal Value: </label>
+                <input 
+                    name="endValue" 
+                    placeholder="What's your desired rate?" 
+                    onChange={""} />
             </form>
         </>
     )
