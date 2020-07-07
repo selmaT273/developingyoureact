62ba168e4d2d598a95c8f3edd80402e57879b36a
cnickels21 <cnickels_21@yahoo.com>
2020-07-07 14:13:15 -0500

    empty form created

--- src/creategoal/index.js
+++ src/creategoal/index.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+export default function CreateGoal(props){
+
+
+    return (
+        <>
+            <h3>Create Your Goal!</h3>
+            <form onSubmit={""}>
+                
+            </form>
+        </>
+    )
+}
