926a082057accd078eeb3053395576e38356e9ac
Marie Antons <chefantons@gmail.com>
2020-07-08 10:56:50 -0500

    detail page layout

--- src/goaldetails/index.js
+++ src/goaldetails/index.js
@@ -3,16 +3,26 @@ import React from 'react';
 import useFetch from '../hooks/fetch';
 
 export default function GoalDetails(props) {
-    const {data} = useFetch('https://developingyouapi.azurewebsites.net/api/goals');
+    const { data } = useFetch('https://developingyouapi.azurewebsites.net/api/goals');
 
     if (!data) {
         return <p>Loading...</p>
     }
 
     return (
+        <h1>{goal.title}</h1>
         <ul>
+            <li>{goal.startDate}</li>
+            <li>{goal.endDate}</li>
+            <li>{goal.category}</li>
             {data.map((goal) => (
-                <li>{goal.title}</li>
+                <li key={goal.id}>
+                    <ul>
+                        {goal.instances.map((instance) => (
+                            <li key={instance.id}>{instance.startTime}  {instance.comment}</li>
+                        ))}
+                    </ul>
+                </li>
             ))}
         </ul>
     )
