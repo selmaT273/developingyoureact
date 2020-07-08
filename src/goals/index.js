3053f89696849cfafa8115e01fc8fdd4fb4cdf34
Stacey Teltser <stacey.teltser@gmail.com>
2020-07-08 10:37:05 -0500

    rendering instances for each goal

--- src/goals/index.js
+++ src/goals/index.js
@@ -5,15 +5,26 @@ import useFetch from '../hooks/fetch';
 export default function Goals(props) {
     const {data} = useFetch('https://developingyouapi.azurewebsites.net/api/goals');
 
+
     if (!data) {
         return <p>Loading...</p>
     }
+    console.log(data);
 
     return (
         <ul>
-            {data.map((goal) => (
-                <li>{goal.title}</li>
+            {data.map((goal) => (               
+                <li key={goal.id}>
+                    {goal.title} {goal.startDate}
+                    <ul>
+                    {goal.instances.map((instance) => (
+                    <li key={instance.id}>{instance.startTime}  {instance.comment}</li>
+                    ))}
+                    </ul>
+                </li>        
             ))}
+
         </ul>
+        
     )
 }
