04cbc6743839c9901b2de189b469ef8fab23ad77
cnickels21 <cnickels_21@yahoo.com>
2020-07-08 14:19:46 -0500

    create goal refactored to class component

--- src/creategoal/index.js
+++ src/creategoal/index.js
@@ -1,44 +1,47 @@
 import React from 'react';
 
-export default function CreateGoal(props){
+export default class CreateGoal extends React.Component{
 
-    return (
-        <>
-            <h3>Create Your Goal!</h3>
-            <form onSubmit={""}>
-                <label for="goal-title">Title: </label>
-                <input 
-                    name="goal-title" 
-                    placeholder="Goal title" 
-                    onChange={""} />
-
-                <label for="start-date">Start Date: </label>
-                <input 
-                    name="start-date" 
-                    type="datetime-local" />
-
-                <label for="end-date">End Date: </label>
-                <input 
-                    name="end-date" 
-                    type="datetime-local" />
-
-                <label for="start-value">Start Value: </label>
-                <input 
-                    name="start-value" 
-                    placeholder="What's your current rate?" 
-                    onChange={""} />
-
-                <label for="target-value">Target Value: </label>
-                <input 
-                    name="target-value" 
-                    placeholder="What's your desired rate?" 
-                    onChange={""} />
-
-                <select>
-                    
-                </select>
-
-            </form>
-        </>
-    )
+    render(){
+        return (
+            <>
+                <h3>Create Your Goal!</h3>
+                <form onSubmit={""}>
+                    <label for="goal-title">Title: </label>
+                    <input 
+                        name="goal-title" 
+                        placeholder="Goal title" 
+                        onChange={""} />
+    
+                    <label for="start-date">Start Date: </label>
+                    <input 
+                        name="start-date" 
+                        type="datetime-local" />
+    
+                    <label for="end-date">End Date: </label>
+                    <input 
+                        name="end-date" 
+                        type="datetime-local" />
+    
+                    <label for="start-value">Start Value: </label>
+                    <input 
+                        name="start-value" 
+                        placeholder="What's your current rate?" 
+                        onChange={""} />
+    
+                    <label for="target-value">Target Value: </label>
+                    <input 
+                        name="target-value" 
+                        placeholder="What's your desired rate?" 
+                        onChange={""} />
+    
+                    <select>
+    
+                    </select>
+    
+                </form>
+            </>
+        )
+    }
+    
 }
