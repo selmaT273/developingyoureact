fdc424f1dc623ab2a7f7bab88f7efabb3a437eff
Marie Antons <chefantons@gmail.com>
2020-07-08 10:30:52 -0500

    instance form created

--- src/createinstance/index.js
+++ src/createinstance/index.js
@@ -1,1 +1,33 @@
 import React from 'react';
+
+const Modal = props => {
+return (
+    <div>
+        <div>
+            <form onSubmit={""}>
+
+               <label > 
+                   Start Time:
+               <input className= "startTime" type="datetime-local"/>
+               </label>
+
+               <label > 
+                   End Time:
+               <input className= "endTime" type="datetime-local"/>
+               </label>
+
+               <label > 
+                  Comment(s):
+               <input className= "comment" type="text"/>
+               </label>
+
+               
+            <label>
+            <input type="submit" value="Add New Instance"/>
+            </label>
+            </form>
+        </div>
+    </div>
+)
+
+}
