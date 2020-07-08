7981f17c0fc09dede0c57aa20addf2cc6d596c34
cnickels21 <cnickels_21@yahoo.com>
2020-07-08 14:18:37 -0500

    date time attribute refactoring

--- src/creategoal/index.js
+++ src/creategoal/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 export default function CreateGoal(props){
 
-
     return (
         <>
             <h3>Create Your Goal!</h3>
@@ -16,10 +15,9 @@ export default function CreateGoal(props){
                 <label for="start-date">Start Date: </label>
                 <input 
                     name="start-date" 
-                    type="datetime-local"
-                    value={props.datetime} />
+                    type="datetime-local" />
 
-                <label for="end-date">Start Date: </label>
+                <label for="end-date">End Date: </label>
                 <input 
                     name="end-date" 
                     type="datetime-local" />
@@ -30,7 +28,7 @@ export default function CreateGoal(props){
                     placeholder="What's your current rate?" 
                     onChange={""} />
 
-                <label for="target-value">Goal Value: </label>
+                <label for="target-value">Target Value: </label>
                 <input 
                     name="target-value" 
                     placeholder="What's your desired rate?" 
--- src/home/index.js
+++ src/home/index.js
@@ -19,6 +19,5 @@ export default function Home(props){
                     </div>
                 ))}
             </div>
-            
         )
 }
