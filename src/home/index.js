0fb45af4852cfecb385f6018cf773bab56375741
Marie Antons <chefantons@gmail.com>
2020-07-08 11:17:51 -0500

    toggle wire up to pop instance modal on click

--- src/home/index.js
+++ src/home/index.js
@@ -1,5 +1,20 @@
-// import React from 'react';
+import React from 'react';
+import Modal from '../createinstance'
 
-export default function Home(){
-    
+
+
+export default class Home extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            showInstanceForm: false,
+        };
+    }
+
+    toggleInstanceForm = () => {
+        this.setState(oldState => ({
+            showInstanceForm: !oldState.showInstanceForm
+        }));
+    }
 }
