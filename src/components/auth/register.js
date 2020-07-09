6c19ccc6a3d2bd5c06e14fd11086b9d16be8769a
cnickels21 <cnickels_21@yahoo.com>
2020-07-09 10:48:54 -0500

    register form created

---
+++ src/components/auth/register.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { AuthContext } from '../../contexts/auth';
+import '../../index.scss';
+
+export default class Register extends React.Component {
+    static contextType = AuthContext;
+
+    handleSubmit = e => {
+        e.preventDefault();
+    }
+
+    render() {
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h3>Start achieving your goals today!</h3>
+                <input placeholder="Username" name="username" required />
+                <br />
+                <br />
+                <input placeholder="Password" type="password" name="password" required />
+                <br />
+                <br />
+                <input placeholder="Email" type="email" name="email" required />
+                <br />
+                <br />
+                <button>Register</button>
+            </form>
+        )
+    }
+}
