2b039ad962237242238becd281704a194b2f103b
Marie Antons <chefantons@gmail.com>
2020-07-09 10:00:19 -0500

    added body

--- src/aboutUs/index.js
+++ src/aboutUs/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 export default function AboutUs() {
     return(
+        <body>
         <div id="aboutUs">
 
         <section class="AboutSection">Developing You is a guided behavioral modification app that helps make the journey of achieving your personal goals more attainable. Created with evidence-based approaches such as S.M.A.R.T. (Specific Measurable Attainable Relevant and Timely) and positive reinforcement, this app is designed to make the process of reaching your goals more easily attainable and feel more rewarding!</section>
@@ -15,7 +16,7 @@ export default function AboutUs() {
 
         <img alt="stacey" src={ require('../../public/assets/StaceyPic.jpg') } />
         <p class="StaceyWords">Greetings! My name is Stacey Teltser and I'm a Software Developer. I am a native of DC and moved to Iowa to pursue my BS at Coe. After graduating I worked as a Sign Language Interpreter for several years, where I was exposed to programming. That led me to learn basic Javascript and Python during my free time. It wasn't long after that I enrolled in DeltaV! I have enjoyed developing new skills, engaging in continuous problem solving, and learning new technologies. I look forward to collaborating with others to create and maintain software that positively impacts the lives of others.</p>
-        
+
         <img alt="francesco" src={ require('../../public/assets/FrancescoPic.jpg') } />
         <p class="FrancescoWords">
             Hello, my name is Francesco Troiano, I am a software developer with previous experience in the customer service industry. In previous jobs I had found my passion, which was for people. I enjoyed working with them, helping them develop, and watching them grow drove me to furthering my own self. In my drive to become a full stack developer I found that I could take my passion and apply it towards the wonderful world of coding, and I'm excited to do so at future opportunities.
@@ -23,5 +24,6 @@ export default function AboutUs() {
 
 
     </div>
+    </body>
       );
 }
