824b77bd1835f0c8a407a16774daf2ce9ba10f99
francescodt <francescodtroiano@gmail.com>
2020-07-09 14:17:15 -0500

    just kidding no reset

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './reset.scss';
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
 import { AuthProvider } from './contexts/auth';
