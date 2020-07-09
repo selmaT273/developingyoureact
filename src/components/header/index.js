b02d0d640919a64b65a7757a331b03e877b98131
francescodt <francescodtroiano@gmail.com>
2020-07-09 15:04:47 -0500

    header icons

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,19 +6,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // import Goals from '../../goals/index';
 
-import useTheme from '../../contexts/theme.js'
+import useTheme from '../../contexts/theme.js';
+import useAuth from '../../contexts/auth.js';
 
 
 export default function Header() {
 
   const theme = useTheme();
+  const auth = useAuth();
     
     return(
       <header className={theme.mode}>
           <div className="account-bar">
           <span className="user-icon"><FontAwesomeIcon icon={faUser} /></span>
           <span className="settings-icon"><FontAwesomeIcon icon={faCog} onClick={() => theme.toggleMode()}/></span>
-          <a href="/Logout"><FontAwesomeIcon icon={faSignOutAlt} /></a>
+          <span className="logout-icon"><FontAwesomeIcon icon={faSignOutAlt}onClick={() => auth.logout()}/></span>
           </div>
           <h1>Developing You</h1>
         <nav>
