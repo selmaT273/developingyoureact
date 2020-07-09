f327c73b2f838dbfd0de37fac467f06232e0da53
francescodt <francescodtroiano@gmail.com>
2020-07-09 11:05:55 -0500

    theme toggle fixed to settings icon

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -15,10 +15,10 @@ export default function Header() {
     
     return(
       <header className={theme.mode}>
-          {/* <button onClick={() => theme.toggleMode()}>{theme.mode} mode</button> */}
+          {/* <button onClick={() => theme.toggleMode()}></button> */}
           <div className="account-bar">
           <span className="user-icon"><FontAwesomeIcon icon={faUser} /></span>
-          <span className="settings-icon"><FontAwesomeIcon icon={faCog} /></span>
+          <span className="settings-icon"><FontAwesomeIcon icon={faCog} onClick={() => theme.toggleMode()} /></span>
           <a href="/Logout"><FontAwesomeIcon icon={faSignOutAlt} /></a>
           </div>
           <h1>Developing You</h1>
