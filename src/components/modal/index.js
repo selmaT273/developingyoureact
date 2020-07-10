2ddc951ba6dc257aa24d3e90f1223d3345ab710b
francescodt <francescodtroiano@gmail.com>
2020-07-09 22:24:35 -0500

    styling added to modal

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import './modal.scss';
 
 
 const Modal = props => {
     const { onClose, children } = props;
 
     return (
-        <div>
-            <div>
-                <button onClick={onClose}>
-                Close
-                </button>
+        <div className="overlay">
+            <div className="modal">
+                <div className="header">
+                    <button onClick={onClose}>
+                    Close
+                    </button>
+                </div>
                 <div className="content">
                     {children}
                 </div>
