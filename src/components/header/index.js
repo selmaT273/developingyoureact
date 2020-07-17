a03df4dff2d28bf2885eeaa8755b0b6e3578d6b8
Stacey Teltser <stacey.teltser@gmail.com>
2020-07-17 14:44:03 -0500

    routing done and preliminary stuff as well as commented out code for now to integrate back in later

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,33 +1,35 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 import './header.scss';
-import { faUser, faCog, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faUser, faCog, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Navbar from './navbar.js';
 
 // import Goals from '../../goals/index';
 
 import useTheme from '../../contexts/theme.js';
-import useAuth from '../../contexts/auth.js';
+// import useAuth from '../../contexts/auth.js';
 
 
 export default function Header() {
 
   const theme = useTheme();
-  const auth = useAuth();
+  // const auth = useAuth();
     
     return(
       <header className={theme.mode}>
-          <div className="account-bar">
+          {/* <div className="account-bar">
           <span className="user-icon"><FontAwesomeIcon icon={faUser} /></span>
           <span className="settings-icon"><FontAwesomeIcon icon={faCog} onClick={() => theme.toggleMode()}/></span>
           <span className="logout-icon"><FontAwesomeIcon icon={faSignOutAlt} onClick={() => auth.logout()}/></span>
-          </div>
-          <h1>Developing You</h1>
-        <nav>
+          </div> */}
+          {/* <h1>Developing You</h1> */}
+          <Navbar/>
+        {/* <nav>
           <a href="/">Home</a>
           <NavLink exact to="/Goals">Goals</NavLink>
           <a href="/About">About</a>
-        </nav>
+        </nav> */}
       </header>
     );
   }
