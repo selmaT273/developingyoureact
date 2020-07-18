9754621c87c4483cc57e6cf012b63cc0e5307ddd
Stacey Teltser <stacey.teltser@gmail.com>
2020-07-18 16:51:32 -0500

    sticky footer and header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Goals from './goals/index';
-import useTheme from './contexts/theme';
 import useAuth from './contexts/auth';
 import Header from './components/header';
 import Footer from './components/footer';
@@ -10,15 +9,17 @@ import Home from './home/index';
 import { If } from './components/if';
 import GoalDetails from './goaldetails/index';
 import About from './aboutUs/index'
+import './components/footer/footer.scss';
+import './index.scss';
 
 function App() {
-  const { mode } = useTheme();
   const { user } = useAuth();
 
   return (
     <>
-    <div className={mode}>
-      <Header />
+    <div className="site">
+    <Header />
+      <div className="site-content">
       <Switch>
         <Route exact path="/">
           <If condition={user === null}>
@@ -38,6 +39,7 @@ function App() {
           <About />
         </Route>
       </Switch>
+      </div>
       <Footer />
      </div>
     </>
